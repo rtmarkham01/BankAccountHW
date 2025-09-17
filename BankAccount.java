eff57fd2f5c8cdbf3109aadf23b40b4da860c024
rtmarkham01 <rtmarkham01@bvsd.org>
2025-09-17 14:14:26 -0600

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,17 @@
 public class BankAccount{
+  private String name;
+  private int accountnum;
+  private double balence;
+  private String pin;
+  private boolean loggedin;
+
+  public BankAccount (String name, int accountnum, double balence, String pin, boolean loggedin){
+    this.name=name;
+    this.accountnum=accountnum
+    this.balence=balence
+    this.pin=pin
+    this.loggedin=false;
+  }
+
   
 }
