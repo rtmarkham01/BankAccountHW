30c913822183d39de283381a3c065572ef570675
rtmarkham01 <rtmarkham01@bvsd.org>
2025-09-22 20:23:50 +0000

    done

--- BankAccount.java
+++ BankAccount.java
@@ -5,22 +5,53 @@ public class BankAccount{
   private String pin;
   private boolean loggedin;
 
-  public BankAccount (String name, int accountnum, double balence, String pin, boolean loggedin){
+  public BankAccount (String name, int accountnum, double balence, String pin){
     this.name=name;
-    this.accountnum=accountnum
-    this.balence=balence
-    this.pin=pin
+    this.accountnum=accountnum;
+    this.balence=balence;
+    this.pin=pin;
     this.loggedin=false;
   }
   public double getBalence(){
     if (loggedin == true)
       return balence;
     else
-      throw new IllegalStateExeption("Login Error")
+      throw new IllegalStateException("Login Error");
   }
+  public void login(String pin){
+    if (pin == this.pin)
+      loggedin = true;
+    else
+      throw new IllegalStateException("Login Error");
+}
+
+
+  public void deposite(double amount){
+    if (loggedin == true)
+      balence += amount;
+    else
+      throw new IllegalStateException("Login Error");
+  }
+  public void withdraw(double amount){
+    if (loggedin == true)
+      balence -= amount;
+    else
+      throw new IllegalStateException("Login Error");
+  }
+public String toString(){
+  return ("Account number: " + accountnum + ", balence: $" + balence);
+}
+
   public static void main(String[] args){
-    BankAccountUser111 account1 = new bankAccount("Checking", 111, 1000.00, "8888");
-    System.out.println()
-    
+    BankAccount account1 = new BankAccount("Checking", 111, 1000.00, "8888");
+    account1.login("8888");
+    System.out.println(account1.getBalence());
+
+    account1.deposite(500.00);
+    System.out.println(account1);
+    account1.withdraw(500.00);
+    System.out.println(account1);
+    System.out.println("YERT");
   }
+
 }
